40f1ab6736ad9f4fd62b67715dea4278b91bc472
saiAkulaGit <60208605+saiAkulaGit@users.noreply.github.com>
2020-10-13 19:50:19 +0530

    Add files via upload

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,5 +1,7 @@
+import os
+import argparse
 class VigenereCipher(object):
-    def __init__(self, key):
+    def __init__(self,key):
         print('Vigenere Cipher Encription')
         self.key=key
     
@@ -41,30 +43,54 @@ def write_to_file(file_name,data):
     f=open(file_name,'w')
     data=f.write(data)
     f.close()
+
+def encode_from_file(file_name,obj):
+    data=read_from_file(file_name)
+    for _ in range(args.strength):
+        data= obj.encode(data)
+    write_to_file(file_name,data)
+    print('encode file -> '+file_name)
+
+def decode_from_file(file_name,obj):
+    data=read_from_file(file_name)
+    for _ in range(args.strength):
+        data= obj.decode(data)
+    write_to_file(file_name,data)    
+    print('decode file -> '+file_name)
+
+def encription_form_path(PATH,obj):
+        try:
+                for path in os.listdir(PATH):
+                        encription_form_path(PATH+'/'+path,obj)
+        except:
+                if(args.encode):
+                        encode_from_file(PATH,obj)
+                elif(args.decode):   
+                        decode_from_file(PATH,obj)
     
-import argparse    
 parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-i','--input_file', help='input file name', required=True)
+parser.add_argument('-i','--input_file', help='input file name', required=False)
 parser.add_argument('-e','--encode', help='encode password', required=False)
 parser.add_argument('-d','--decode', help='decode password', required=False)
-parser.add_argument('-s','--strength', help='strength of encription', type=int, default=1, required=False)
-args = vars(parser.parse_args())
+parser.add_argument('-f','--folder',help='folder name',required=False)
+parser.add_argument('-s','--strength', help='encription strength', type=int, default=1, required=False)
+args =(parser.parse_args())
 
-data=read_from_file(args.input_file)
+if(args.input_file):
+    PATH=args.input_file
+elif(args.folder):
+    PATH=args.folder
+else:
+    exit('Need --input_file or --folder\nUse -h for help')
 
 if(args.encode):
-    obj=VigenereCipher(args.encode)
-    for _ in range(args.strength):
-        data=obj.encode(data)
-    write_to_file('encode.txt',data)
-    print('written into file encode.txt')  
+    pswd=args.encode
 elif(args.decode):
-    obj=VigenereCipher(args.decode)
-    for _ in range(args.strength):
-        data=obj.encode(data)
-    write_to_file('decode.txt',data)    
-    print('written into file decode.txt')
+    pswd=args.decode
 else:
-    print('Need --encode or --decode password \nUse -h for help')
+    exit('Need --encode or --decode\nUse -h for help')
 
+
+obj=VigenereCipher(pswd)
+encription_form_path(PATH,obj)
 
