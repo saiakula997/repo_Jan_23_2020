7b7f8b44fd82c7719f0b8b510919fc754060af38
Just4Share <justforshare7@gmail.com>
2023-09-25 09:02:09 -0500

    Changed processor to Cortex-A53 ARMv7 Arch

--- startup/build_commands.sh
+++ startup/build_commands.sh
@@ -1,3 +1,3 @@
-arm-none-eabi-as -mcpu=cortex-a15 startup.s -o startup.o
-arm-none-eabi-gcc -c -mcpu=cortex-a15 entry.c -o entry.o
+arm-none-eabi-as -mcpu=cortex-a53 startup.s -o startup.o
+arm-none-eabi-gcc -c -mcpu=cortex-a53 entry.c -o entry.o
 arm-none-eabi-ld -T boot.ld entry.o startup.o -o output.elf
