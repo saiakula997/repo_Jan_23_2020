2dc63dcad5a5bf13b70b746c21f02dca28b29fa6
Just4Share <justforshare7@gmail.com>
2023-10-14 00:44:33 -0500

    p_scheduler

--- aos_project/p_scheduler/startup.s
+++ aos_project/p_scheduler/startup.s
@@ -8,7 +8,8 @@
 _MyApp:
     MOV R2, #0xABCD
     MRC p15, 0, R0, c12, c0, 0
-    MCR p15, 0, R1, c12, c0, 0
+    MCR p15, 0, R2, c12, c0, 0
+    MRC p15, 0, R0, c12, c0, 0
     B   Reset_Handler
     B   NMI_Handler
     B   SVC_Handler
