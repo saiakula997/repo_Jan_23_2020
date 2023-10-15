4f8eef040fb22fe1953526cac38edc3bd430fed1
Just4Share <justforshare7@gmail.com>
2023-10-14 19:37:48 -0500

    p_scheduler

--- aos_project/p_scheduler/startup.s
+++ aos_project/p_scheduler/startup.s
@@ -2,6 +2,7 @@
 .global delay
 .global panic 
 .global heap_start
+.global ttb_address
 
 .text
 .align 2
@@ -17,9 +18,10 @@ Reset_Handler:
     MRC p15, 0, R0, c12, c0, 0
     MCR p15, 0, R2, c12, c0, 0
     MRC p15, 0, R0, c12, c0, 0
-    MOV R0, #0x01
+    LDR R0, =heap_start
     SVC 0
     MOV R1, #0x01
+
     B entry
     B .
     
@@ -51,3 +53,42 @@ _delay_loop:
     CMP R1, #0
     BNE _ploop
     BX LR
+
+creating_pt:
+    MRC p15, 0, r1, c1, c0, 1
+    ORR r1, r1, #(0x1 <<2)
+    MCR p15, 0, r1, c1, c0, 1 ;
+    DSB
+    ISB
+descriptor:
+    LDR r1, =ttb_address
+    LDR r3, = 4095
+write_pte:
+    ORR r2, r0, r3, LSL #20
+    STR r2, [r1, r3, LSL #2]
+    SUBS r3, r3, #1
+    BNE write_pte
+    BIC r0, r0, #12 
+    ORR r0, r0, #4 
+    BIC r0, r0, #28672 
+    ORR r0, r0, #20480 
+    ORR r0, r0, #65536 
+    STR r0, [r1]
+    
+    @ Initialize MMU
+    MOV r1,#0x0
+    MCR p15, 0, r1, c2, c0, 2 
+    LDR r1, =ttb_address
+
+    MCR p15, 0, r1, c2, c0, 0 
+    @ Write Translation Table Base Register 0
+    @ In this simple example, we don't use TRE or Normal Memory Remap Register. 
+    @ Set all Domains to Client
+    LDR r1, =0x55555555
+    MCR p15, 0, r1, c3, c0, 0 
+    
+    @ Write Domain Access Control Register
+    @ Enable MMU
+    MRC p15, 0, r1, c1, c0, 0 @ Read Control Register configuration data 
+    ORR r1, r1, #0x1 @ Bit 0 is the MMU enable
+    MCR p15, 0, r1, c1, c0, 0
